ae7cb6e98751a4b819a6923e63bda3bb7e022288
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-01-24 09:19:17 +0100

    use deduplicated sequences for both forward and reverse complement sequences

--- inc/readstorage.hpp
+++ inc/readstorage.hpp
@@ -18,16 +18,14 @@ struct ReadStorage{
     std::vector<std::vector<std::string>> headers;
     std::vector<std::vector<std::string>> qualityscores;
     std::vector<std::vector<std::string>> reverseComplqualityscores;
+
     std::vector<std::vector<Sequence>> sequences;
     std::vector<std::vector<Sequence>> reverseComplSequences;
+
     std::vector<Sequence*> sequencepointers;
-    std::vector<Sequence> sequences_dedup;
     std::vector<Sequence*> reverseComplSequencepointers;
-    std::vector<Sequence> reverseComplSequences_dedup;
 
-    std::vector<std::uint64_t> bytecounts;
-
-    //std::vector<std::unordered_map<std::string>> dedupqscores;
+    std::vector<Sequence> all_unique_sequences; //forward and reverse complement
 
     ReadStorage();
 
@@ -51,6 +49,8 @@ struct ReadStorage{
     const std::string* fetchReverseComplementQuality_ptr(std::uint32_t readNumber) const;
     const Sequence* fetchSequence_ptr(std::uint32_t readNumber) const;
     const Sequence* fetchReverseComplementSequence_ptr(std::uint32_t readNumber) const;
+
+    double getMemUsageMB();
 };
 
 
--- src/readstorage.cpp
+++ src/readstorage.cpp
@@ -20,8 +20,6 @@
 		reverseComplqualityscores.resize(nThreads);
 		sequences.resize(nThreads);
 		reverseComplSequences.resize(nThreads);
-		bytecounts.resize(nThreads);
-		//dedupqscores.resize(nThreads);
 	}
 
 	ReadStorage::ReadStorage(const ReadStorage& other){
@@ -35,7 +33,11 @@
 		nThreads = other.nThreads;
 		sequences = other.sequences;
 		reverseComplSequences = other.reverseComplSequences;
-		bytecounts = other.bytecounts;
+
+		sequencepointers = other.sequencepointers;
+		reverseComplSequencepointers = other.reverseComplSequencepointers;
+		all_unique_sequences = other.all_unique_sequences;
+
 		return *this;
 	}
 
@@ -46,7 +48,11 @@
 		sequences = std::move(other.sequences);
 		reverseComplSequences = std::move(other.reverseComplSequences);
 		nThreads = other.nThreads;
-		bytecounts = other.bytecounts;
+
+		sequencepointers = std::move(other.sequencepointers);
+		reverseComplSequencepointers = std::move(other.reverseComplSequencepointers);
+		all_unique_sequences = std::move(other.all_unique_sequences);
+
 		return *this;
 	}
 
@@ -61,7 +67,11 @@
 		    a.clear();
 		for(auto& a : reverseComplqualityscores)
 		    a.clear();
-		bytecounts.clear();
+
+		sequencepointers.clear();
+		reverseComplSequencepointers.clear();
+		all_unique_sequences.clear();
+
 		isReadOnly = false;
 	}
 
@@ -78,14 +88,8 @@
 		sequences[id].resize(indexInVector+1);
 		reverseComplSequences[id].resize(indexInVector+1);
 
-		//if sequence does not contain 'N' , it can be stored in a compressed format using two bits per base
-		bool hasN = std::any_of(read.sequence.begin(), read.sequence.end(), [](char c){return c == 'N';});
-
-		Sequence seq{read.sequence, !hasN}; 
+		Sequence seq{read.sequence, true};  
 		std::string q(read.quality);
-
-		bytecounts[id] += 2*seq.getNumBytes(); //sequence + reverse complement
-		bytecounts[id] += 2*read.quality.size(); //quality scores + reverse complement qscores 
 
 		headers[id][indexInVector] = std::move(read.header);
 		qualityscores[id][indexInVector] = std::move(read.quality);
@@ -95,10 +99,8 @@
 
 		reverseComplSequences[id][indexInVector] = (seq.reverseComplement());
 		sequences[id][indexInVector] = (seq);
-
 	}
 
-#if 1
 	void ReadStorage::noMoreInserts(){
 		isReadOnly = true;
 
@@ -108,146 +110,62 @@
 
 		if(nSequences == 0) return;
 
-		sequences_dedup.resize(nSequences);
-		reverseComplSequences_dedup.resize(nSequences);
+		std::vector<Sequence> sequencesflat;
+		sequencesflat.reserve(2*nSequences);
 
 		for(int readnum = 0; readnum < nSequences; readnum++){
 			std::uint32_t vecnum = readnum % nThreads;
 			int indexInVector = readnum / nThreads;
 
-			sequences_dedup[readnum] = std::move(sequences[vecnum].at(indexInVector));
-			reverseComplSequences_dedup[readnum] = std::move(reverseComplSequences[vecnum].at(indexInVector));
+			sequencesflat.push_back(std::move(sequences[vecnum].at(indexInVector)));
 		}
+		for(auto& vec : sequences)
+			vec.clear();
 
-		std::vector<Sequence> tmp(sequences_dedup.begin(), sequences_dedup.end());
-		std::vector<Sequence> revtmp(reverseComplSequences_dedup.begin(), reverseComplSequences_dedup.end());
+		for(int readnum = 0; readnum < nSequences; readnum++){
+			std::uint32_t vecnum = readnum % nThreads;
+			int indexInVector = readnum / nThreads;
 
-		sequences.clear();
-		reverseComplSequences.clear();
+			sequencesflat.push_back(std::move(reverseComplSequences[vecnum].at(indexInVector)));
+		}
+		for(auto& vec : reverseComplSequences)
+			vec.clear();
 
-		sequencepointers.resize(nSequences, nullptr);
-		reverseComplSequencepointers.resize(nSequences, nullptr);
+		std::vector<Sequence> tmp(sequencesflat);
 
-		std::sort(sequences_dedup.begin(), sequences_dedup.end());
-		auto uniqueend = std::unique(sequences_dedup.begin(), sequences_dedup.end());
+		std::sort(sequencesflat.begin(), sequencesflat.end());
+		sequencesflat.erase(std::unique(sequencesflat.begin(), sequencesflat.end()), sequencesflat.end());
 
 		std::map<const Sequence, int> seqToSortedIndex;
 
-		int index = 0;
-		for(auto it = sequences_dedup.begin(); it != uniqueend; it++){
-			seqToSortedIndex[(*it)] = index;
-			index++;
+		for(const auto& s : sequencesflat){
+			seqToSortedIndex[s] = &s - sequencesflat.data();
 		}
 
-		std::cout << "found " << std::distance(uniqueend, sequences_dedup.end()) << " duplicate reads\n";
-		sequences_dedup.resize(std::distance(sequences_dedup.begin(), uniqueend));
-		sequences_dedup.shrink_to_fit();
+		assert(sequencesflat.size() == seqToSortedIndex.size());
 
+		std::cout << "ReadStorage: found " << (2 * nSequences - sequencesflat.size()) << " duplicates\n";
+
+		
+
+		sequencepointers.resize(nSequences);
 		for(int i = 0; i < nSequences; i++)
-			sequencepointers[i] = &sequences_dedup[seqToSortedIndex[tmp[i]]];
+			sequencepointers[i] = &sequencesflat[seqToSortedIndex[tmp[i]]];
+
+		reverseComplSequencepointers.resize(nSequences);
+		for(int i = 0; i < nSequences; i++)
+			reverseComplSequencepointers[i] = &sequencesflat[seqToSortedIndex[tmp[nSequences + i]]];
 	
 		//check
 		for(int i = 0; i < nSequences; i++){
 			assert(*sequencepointers[i] == tmp[i] && "readstorage wrong sequence after dedup");
 		}
-
-		std::sort(reverseComplSequences_dedup.begin(), reverseComplSequences_dedup.end());
-		uniqueend = std::unique(reverseComplSequences_dedup.begin(), reverseComplSequences_dedup.end());
-
-		seqToSortedIndex.clear();
-
-		index = 0;
-		for(auto it = reverseComplSequences_dedup.begin(); it != uniqueend; it++){
-			seqToSortedIndex[(*it)] = index;
-			index++;
-		}
-
-		std::cout << "found " << std::distance(uniqueend, reverseComplSequences_dedup.end()) << " duplicate reads\n";
-		reverseComplSequences_dedup.resize(std::distance(reverseComplSequences_dedup.begin(), uniqueend));
-		reverseComplSequences_dedup.shrink_to_fit();
-
-		for(int i = 0; i < nSequences; i++)
-			reverseComplSequencepointers[i] = &reverseComplSequences_dedup[seqToSortedIndex[revtmp[i]]];
-	
-		//check
 		for(int i = 0; i < nSequences; i++){
-			assert(*reverseComplSequencepointers[i] == revtmp[i] && "readstorage wrong sequence after dedup");
+			assert(*reverseComplSequencepointers[i] == tmp[nSequences + i] && "readstorage wrong sequence after dedup");
 		}
+
+		all_unique_sequences = std::move(sequencesflat);
 	}
-
-#else
-
-	void ReadStorage::noMoreInserts(){
-		isReadOnly = true;
-
-		int nSequences = 0;
-		for(const auto& v : sequences)
-			nSequences += v.size();
-
-		if(nSequences == 0) return;
-
-		auto make_index_buffer = [&]{
-			std::vector<size_t> v(nSequences);
-			std::iota(v.begin(), v.end(), size_t(0));
-			return v;
-		};
-
-		auto dedup = [&](auto& what, auto& uniqueobj, auto& ptrs){
-
-			uniqueobj.resize(nSequences);
-
-			for(int readnum = 0; readnum < nSequences; readnum++){
-				std::uint32_t vecnum = readnum % nThreads;
-				int indexInVector = readnum / nThreads;
-
-				uniqueobj[readnum] = std::move(what[vecnum].at(indexInVector));
-			}
-
-			// build a buffer of unique element indexes:
-			auto uniques = make_index_buffer();
-
-			// compares indexes by their object: 
-			auto index_less = [&](auto lhs, auto rhs) { return uniqueobj[lhs] < uniqueobj[rhs]; };
-			auto index_equal = [&](auto lhs, auto rhs) { return uniqueobj[lhs] == uniqueobj[rhs]; };
-
-			std::sort( uniques.begin(), uniques.end(), index_less );
-			uniques.erase(std::unique( uniques.begin(), uniques.end(), index_equal), uniques.end() );
-
-			// build table of index to unique index:
-			std::map<size_t, size_t, decltype(index_less)> table(index_less);
-			for (size_t& i : uniques){
-				table[i] = &i-uniques.data();
-			}
-
-			// list of index to unique index for each element:
-			auto indexes = make_index_buffer();
-
-			// make indexes unique:
-			for (size_t& i:indexes)
-				i = table[i];
-
-			// build unique object list:
-			std::vector<Sequence> objects;
-			objects.reserve( uniques.size() );
-			for (size_t i : uniques)
-				objects.push_back( std::move(uniqueobj[i]) );
-
-			// build pointer objects:
-			std::vector<Sequence*> ptrarray; // N elements
-			ptrarray.reserve( indexes.size() );
-			for (size_t i : indexes)
-				ptrarray.push_back( std::addressof( objects[i] ) );
-
-			uniqueobj = std::move(objects);
-			ptrs = std::move(ptrarray);
-		};
-
-		dedup(sequences, sequences_dedup, sequencepointers);
-		dedup(reverseComplSequences, reverseComplSequences_dedup, reverseComplSequencepointers);
-	}
-
-
-#endif
 
 	Read ReadStorage::fetchRead(std::uint32_t readNumber) const{
 		Read returnvalue;
@@ -324,5 +242,45 @@
 		return &(reverseComplSequences[id][indexInVector]);		
 #endif
 		}
+	}
+
+	double ReadStorage::getMemUsageMB(){
+		size_t bytes = 0;
+
+		for(const auto& vec : sequences){
+			for(const auto& s : vec){
+				bytes += s.getNumBytes();
+			}
+		}
+
+		for(const auto& vec : reverseComplSequences){
+			for(const auto& s : vec){
+				bytes += s.getNumBytes();
+			}
+		}
+
+		for(const auto& s : all_unique_sequences){
+			bytes += s.getNumBytes();
+		}
+
+		for(const auto& vec : headers){
+			for(const auto& s : vec){
+				bytes += s.size();
+			}
+		}
+
+		for(const auto& vec : qualityscores){
+			for(const auto& s : vec){
+				bytes += s.size();
+			}
+		}
+
+		for(const auto& vec : reverseComplqualityscores){
+			for(const auto& s : vec){
+				bytes += s.size();
+			}
+		}
+
+		return bytes / 1024. / 1024.;
 	}
 
