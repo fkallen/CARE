410d48cf2ed59c9c596e953dd9e711926c8d3ae2
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-02-09 12:54:05 +0100

    sequence struct with max length

--- inc/read.hpp
+++ inc/read.hpp
@@ -14,6 +14,171 @@
 #include <cstring>
 #include <cassert>
 
+#if 1
+template<int MAX_LEN_>
+struct FixedSizeSequence{
+	static constexpr uint8_t BITS_PER_BASE = 2;
+	static constexpr uint8_t BASES_PER_BYTE = (sizeof(uint8_t)*8 / BITS_PER_BASE);
+
+	static constexpr int MAX_LEN = MAX_LEN_;
+	static constexpr int DATA_BYTES = SDIV(MAX_LEN, 4);
+
+	uint8_t data[DATA_BYTES];
+	int length = 0;
+
+	HOSTDEVICEQUALIFIER
+	constexpr int getNumBytes() const{
+		return DATA_BYTES;
+	}
+
+	HOSTDEVICEQUALIFIER
+	int getNbases() const{
+		return length;
+	}
+
+	HOSTDEVICEQUALIFIER
+	FixedSizeSequence(){
+		memset(data, 0, DATA_BYTES);
+	}
+
+	FixedSizeSequence(const std::string& sequence) : length(sequence.size()){
+		if(sequence.size() > MAX_LEN || !encode(sequence.c_str(), sequence.size(), sequence.size(), data, DATA_BYTES, true)){
+			throw std::runtime_error("could not encode sequence");
+		}
+	}
+
+	FixedSizeSequence(const uint8_t* rawdata, int nBases_) : length(nBases_){
+		if(nBases_ > MAX_LEN){
+			throw std::runtime_error("could not encode sequence");
+		}
+		std::memcpy(data, rawdata, sizeof(uint8_t) * SDIV(nBases_,4));
+	}
+
+	HOSTDEVICEQUALIFIER
+	FixedSizeSequence(const FixedSizeSequence& other){
+		*this = other;
+	}
+
+	HOSTDEVICEQUALIFIER
+	FixedSizeSequence& operator=(const FixedSizeSequence& other){
+		length = other.length;
+		memcpy(data, other.data, sizeof(uint8_t) * DATA_BYTES);
+	}
+
+	HOSTDEVICEQUALIFIER
+	bool operator==(const FixedSizeSequence& rhs) const
+	{
+		if(getNbases() != rhs.getNbases()) return false;
+		return (memcmp(begin(), rhs.begin(), getNumBytes()) == 0);
+	}
+
+	HOSTDEVICEQUALIFIER
+	bool operator!=(const FixedSizeSequence& other) const
+	{
+		return !(*this == other);
+	}
+
+	bool operator==(const std::string& other) const
+	{
+		return toString() == other;
+	}
+
+	bool operator!=(const std::string& other) const
+	{
+		return !(*this == other);
+	}
+
+	HOSTDEVICEQUALIFIER
+	bool operator<(const FixedSizeSequence& rhs) const{
+		const int bases = getNbases();
+		const int otherbases = rhs.getNbases();
+		if(bases < otherbases) return true;
+		if(bases > otherbases) return false;
+
+		return (memcmp(begin(), rhs.begin(), getNumBytes()) < 0);
+	}
+
+	HOSTDEVICEQUALIFIER
+	char operator[](int i) const{
+                const int FIRST_USED_BYTE = getNumBytes() - SDIV(getNbases(), BASES_PER_BYTE);
+                const int UNUSED_BYTE_SPACE = (BASES_PER_BYTE - (getNbases() % BASES_PER_BYTE)) % BASES_PER_BYTE;
+		const int byte = FIRST_USED_BYTE + (i + UNUSED_BYTE_SPACE) / BASES_PER_BYTE;
+		const int basepos = (i + UNUSED_BYTE_SPACE) % BASES_PER_BYTE;
+
+		const uint8_t bits = (data[byte] >> (3-basepos) * 2) & 0x03;
+		if(bits == 0x00) return 'A';
+		if(bits == 0x01) return 'C';
+		if(bits == 0x02) return 'G';
+		if(bits == 0x03) return 'T';
+		return '_';
+	}
+
+	HOSTDEVICEQUALIFIER
+	void setBase(int pos, char base){
+                const int FIRST_USED_BYTE = getNumBytes() - SDIV(getNbases(), BASES_PER_BYTE);
+                const int UNUSED_BYTE_SPACE = (BASES_PER_BYTE - (getNbases() % BASES_PER_BYTE)) % BASES_PER_BYTE;
+		const int byte = FIRST_USED_BYTE + (pos + UNUSED_BYTE_SPACE) / BASES_PER_BYTE;
+		const int basepos = (pos + UNUSED_BYTE_SPACE) % BASES_PER_BYTE;
+
+		uint8_t mask = 0x03 << (3-basepos)*2;
+		uint8_t newbyte = data[byte] & ~mask;
+
+                switch(base) {
+                case 'A':
+                        newbyte |=  0x00 << ((3-basepos) * 2);
+                        break;
+                case 'C':
+                        newbyte |=  0x01 << ((3-basepos) * 2);
+                        break;
+                case 'G':
+                        newbyte |=  0x02 << ((3-basepos) * 2);
+                        break;
+                case 'T':
+                        newbyte |=  0x03 << ((3-basepos) * 2);
+                        break;
+		default:printf("error setBase\n");
+                        newbyte |=  0x00 << ((3-basepos) * 2);
+                        break;
+		}
+		data[byte] = newbyte;
+	}
+
+	std::string toString() const{
+		char buf[MAX_LEN+1];
+		bool b = decode(data, getNumBytes(), getNbases(), buf, getNbases()+1);
+		if(!b)
+			throw std::runtime_error("could not decode sequence");
+		return std::string(buf);
+	}
+
+	FixedSizeSequence reverseComplement() const{
+		FixedSizeSequence revcompl;
+		revcompl.length = getNbases();
+
+		bool res = encoded_to_reverse_complement_encoded(begin(), getNumBytes(), revcompl.begin(), getNumBytes(), getNbases());
+		if(!res)
+			throw std::runtime_error("could not get reverse complement of " + toString());
+		
+		return revcompl;
+	}
+
+	HOSTDEVICEQUALIFIER
+	constexpr bool isCompressed() const{
+		return true;
+	}
+
+	HOSTDEVICEQUALIFIER
+	uint8_t* begin() const{
+		return const_cast<uint8_t*>(&data[0]);
+	}
+
+	HOSTDEVICEQUALIFIER
+	uint8_t* end() const{
+		return &data[0] + getNumBytes();
+	}
+};
+#endif
+
 struct Sequence {
 
 	Sequence();
