161246d21e0d75e143673dc6958b1144969b2e95
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-04-27 15:20:19 +0200

    r

--- run.sh
+++ run.sh
@@ -12,11 +12,11 @@ threads=16
 #inputfile=/home/fekallen/arbeit/evaluationtool/datasets/E.coli_SRR1191655_1M.fastq
 #coverage=21
 
-inputfile=/home/fekallen/arbeit/evaluationtool/datasets/E.coli_SRR1191655.fastq
-coverage=255
+#inputfile=/home/fekallen/arbeit/evaluationtool/datasets/E.coli_SRR1191655.fastq
+#coverage=255
 
-#inputfile=/home/fekallen/arbeit/evaluationtool/datasets/C.elegans_SRX218989.fastq
-#coverage=31
+inputfile=/home/fekallen/arbeit/evaluationtool/datasets/C.elegans_SRX218989.fastq
+coverage=31
 
 #inputfile=/home/fekallen/arbeit/evaluationtool/datasets/D.melanogaster_SRR823377.fastq
 #coverage=52
@@ -46,7 +46,7 @@ useQualityScores=--useQualityScores=false
 candidateCorrection=--candidateCorrection=true
 
 #if indels should be corrected, too
-indels=--indels=true
+indels=--indels=false
 
 #minhashing parameters
 #kmer length
