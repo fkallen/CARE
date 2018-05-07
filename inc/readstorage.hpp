b57d050fd5bcf78f75e662e7c4272f34631a0f5f
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-05-07 09:20:35 +0200

    make readstorage with does not store reverse complements

--- inc/readstorage.hpp
+++ inc/readstorage.hpp
@@ -15,9 +15,11 @@ namespace care{
 template<class sequence_t,
 		 class readId_t>
 struct ReadStorage{
-	
+
 	using Sequence_t = sequence_t;
 	using ReadId_t = readId_t;
+
+    static constexpr bool has_reverse_complement = true;
 
 	bool useQualityScores = false;
     bool isReadOnly = false;
@@ -194,6 +196,157 @@ struct ReadStorage{
 			for(size_t i = 0; i < nSequences; i++){
 				assert(*reverseComplSequencepointers[i] == sequencepointers[i]->reverseComplement() && "readstorage wrong reverse complement after dedup");
 			}
+		}
+//TIMERSTOPCPU(READ_STORAGE_CHECK);
+#endif
+
+	}
+
+};
+
+
+
+/*
+    Data structure to store sequences and their quality scores
+*/
+template<class sequence_t,
+		 class readId_t>
+struct ReadStorageMinMemory{
+
+	using Sequence_t = sequence_t;
+	using ReadId_t = readId_t;
+
+    static constexpr bool has_reverse_complement = false;
+
+	bool useQualityScores = false;
+    bool isReadOnly = false;
+
+    std::vector<std::string> qualityscores;
+    std::vector<Sequence_t> sequences;
+    std::vector<Sequence_t*> sequencepointers;
+
+    ReadStorage() : ReadStorage(false){}
+    ReadStorage(bool b) : useQualityScores(b){}
+
+	void init(ReadId_t nReads){
+		clear();
+
+		sequences.resize(nReads);
+        if(useQualityScores){
+            qualityscores.resize(nReads);
+        }
+	}
+
+	void clear(){
+		qualityscores.clear();
+		sequences.clear();
+		sequencepointers.clear();
+
+		isReadOnly = false;
+	}
+
+	void destroy(){
+		clear();
+		qualityscores.shrink_to_fit();
+		sequencepointers.shrink_to_fit();
+	}
+
+    void insertRead(ReadId_t readNumber, const std::string& sequence){
+		if(useQualityScores){
+			insertRead(readNumber, sequence, std::string(sequence.length(), 'A'));
+		}else{
+			Sequence_t seq(sequence);
+			sequences[readNumber] = std::move(seq);
+		}
+	}
+
+    void insertRead(ReadId_t readNumber, const std::string& sequence, const std::string& quality){
+		if(isReadOnly) throw std::runtime_error("cannot insert read into ReadStorage after calling transform()");
+
+		Sequence_t seq(sequence);
+
+		sequences[readNumber] = std::move(seq);
+		if(useQualityScores){
+			qualityscores[readNumber] = std::move(quality);
+		}
+	}
+
+	const std::string* fetchQuality_ptr(ReadId_t readNumber) const{
+		if(useQualityScores){
+			return &(qualityscores[readNumber]);
+		}else{
+			return nullptr;
+		}
+	}
+
+	//Must call transform() beforehand
+	const Sequence_t* fetchSequence_ptr(ReadId_t readNumber) const{
+		return sequencepointers[readNumber];
+	}
+
+	void transform(){
+        if(isReadOnly)
+            return;
+
+		isReadOnly = true;
+
+		std::size_t nSequences = sequences.size();
+
+		if(nSequences == 0) return;
+
+        std::vector<Sequence_t> tmp(sequences);
+        sequences.reserve(2*nSequences);
+
+//TIMERSTARTCPU(READ_STORAGE_SORT);
+        std::sort(sequences.begin(), sequences.end());
+        sequences.erase(std::unique(sequences.begin(), sequences.end()), sequences.end());
+//TIMERSTOPCPU(READ_STORAGE_SORT);
+
+        std::map<const Sequence_t, int> seqToSortedIndex;
+		std::vector<std::map<const Sequence_t, int>> seqToSortedIndextmpvec;
+
+//TIMERSTARTCPU(READ_STORAGE_MAKE_MAP);
+        #pragma omp parallel
+        {
+            int threadId = omp_get_thread_num();
+            #pragma omp single
+            seqToSortedIndextmpvec.resize(omp_get_num_threads()-1);
+
+            #pragma omp barrier
+
+            auto& mymap = threadId == 0 ? seqToSortedIndex : seqToSortedIndextmpvec[threadId-1];
+            #pragma omp for
+            for(std::size_t i = 0; i < sequences.size(); i++){
+                const auto& sequence = sequences[i];
+                mymap[sequence] = &sequence - sequences.data();
+            }
+        }
+
+        for(auto& tmpmap : seqToSortedIndextmpvec){
+            seqToSortedIndex.insert(tmpmap.begin(), tmpmap.end());
+            tmpmap.clear();
+        }
+        seqToSortedIndextmpvec.clear();
+
+//TIMERSTOPCPU(READ_STORAGE_MAKE_MAP);
+
+        assert(sequences.size() == seqToSortedIndex.size());
+
+//TIMERSTARTCPU(READ_STORAGE_MAKE_FWD_POINTERS);
+		sequencepointers.resize(nSequences);
+        #pragma omp parallel for
+		for(size_t i = 0; i < nSequences; i++)
+            sequencepointers[i] = &sequences[seqToSortedIndex[tmp[i]]];
+//TIMERSTOPCPU(READ_STORAGE_MAKE_FWD_POINTERS);
+
+		seqToSortedIndex.clear();
+
+#if 1
+//TIMERSTARTCPU(READ_STORAGE_CHECK);
+		//check
+		#pragma omp parallel for
+		for(size_t i = 0; i < nSequences; i++){
+			assert(*sequencepointers[i] == tmp[i] && "readstorage wrong sequence after dedup");
 		}
 //TIMERSTOPCPU(READ_STORAGE_CHECK);
 #endif
