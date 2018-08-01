c693a29bf75ec257fb8f9ecbf78f224b62967dac
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-08-01 11:21:49 +0200

    return empty range if threshold is reached

--- inc/util.hpp
+++ inc/util.hpp
@@ -1,11 +1,54 @@
 #ifndef CARE_UTIL_HPP
 #define CARE_UTIL_HPP
 
-#include <iterator>
-#include <cstdint>
-#include <algorithm>
-#include <iostream>
 
+#include <algorithm>
+#include <cstdint>
+#include <iterator>
+
+/*
+    Essentially performs std::set_union(first1, last1, first2, last2, d_first)
+    but limits the allowed result size to n.
+    If the result would contain more than n elements, d_first is returned, i.e. the result range is empty
+*/
+template<class InputIt1, class InputIt2, class OutputIt>
+OutputIt set_union_n_or_empty(InputIt1 first1, InputIt1 last1,
+                   InputIt2 first2, InputIt2 last2,
+                   std::size_t n,
+                   OutputIt d_first){
+
+    const OutputIt d_first_old = d_first;
+
+    for (; first1 != last1 && n > 0; ++d_first, --n) {
+        if (first2 == last2){
+            const std::size_t remaining = std::distance(first1, last1);
+            if(remaining > n)
+                return d_first_old;
+            else
+                return std::copy_n(first1, std::min(remaining, n), d_first);
+        }
+
+        if (*first2 < *first1) {
+            *d_first = *first2++;
+        } else {
+            *d_first = *first1;
+            if (!(*first1 < *first2))
+                ++first2;
+            ++first1;
+        }
+    }
+
+    const std::size_t remaining = std::distance(first2, last2);
+    if(remaining > n)
+        return d_first_old;
+    else
+        return std::copy_n(first2, std::min(remaining, n), d_first);
+}
+
+/*
+    Essentially performs std::set_union(first1, last1, first2, last2, d_first)
+    but limits the allowed result size to n.
+*/
 template<class InputIt1, class InputIt2, class OutputIt>
 OutputIt set_union_n(InputIt1 first1, InputIt1 last1,
                    InputIt2 first2, InputIt2 last2,
