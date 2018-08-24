df97585f1ac103bb92ac068dc62ffef8f491cd90
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-08-24 09:51:03 +0200

    add new random forests

--- inc/dmelanogastersrr82337.hpp
+++ inc/dmelanogastersrr82337.hpp
@@ -17,6 +17,35 @@ namespace dmelanogaster_srr82337{
         double mean_col_support, double mean_col_coverage,
         double median_col_support, double median_col_coverage,
         double maxgini);
+
+    bool shouldCorrect(double position_support,
+                        double position_coverage,
+                        double alignment_coverage,
+                        double dataset_coverage,
+                        double min_support,
+                        double min_coverage,
+                        double max_support,
+                        double max_coverage,
+                        double mean_support,
+                        double mean_coverage,
+                        double median_support,
+                        double median_coverage,
+                        double maxgini);
+
+    std::pair<int, int> shouldCorrect_forest(double position_support,
+                        double position_coverage,
+                        double alignment_coverage,
+                        double dataset_coverage,
+                        double min_support,
+                        double min_coverage,
+                        double max_support,
+                        double max_coverage,
+                        double mean_support,
+                        double mean_coverage,
+                        double median_support,
+                        double median_coverage,
+                        double maxgini);
+
 }
 
 #endif
--- inc/ecolisrr490124.hpp
+++ inc/ecolisrr490124.hpp
@@ -16,6 +16,35 @@ namespace ecoli_srr490124{
                 double mean_col_support, double mean_col_coverage,
                 double median_col_support, double median_col_coverage,
                 double maxgini);
+
+
+    bool shouldCorrect(double position_support,
+                        double position_coverage,
+                        double alignment_coverage,
+                        double dataset_coverage,
+                        double min_support,
+                        double min_coverage,
+                        double max_support,
+                        double max_coverage,
+                        double mean_support,
+                        double mean_coverage,
+                        double median_support,
+                        double median_coverage,
+                        double maxgini);
+
+    std::pair<int, int> shouldCorrect_forest(double position_support,
+                        double position_coverage,
+                        double alignment_coverage,
+                        double dataset_coverage,
+                        double min_support,
+                        double min_coverage,
+                        double max_support,
+                        double max_coverage,
+                        double mean_support,
+                        double mean_coverage,
+                        double median_support,
+                        double median_coverage,
+                        double maxgini);
 }
 
 #endif
