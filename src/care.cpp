a2412101e683f6b458d385d8d189b7152b17f1a0
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-08-29 16:03:23 +0200

    remove print statement

--- src/care.cpp
+++ src/care.cpp
@@ -71,8 +71,6 @@ void correctFile_impl(const MinhashOptions& minhashOptions,
     std::cout << "reads take up " << toGB(readStorage.size()) << " GB." << std::endl;
     std::cout << "hash maps take up " << toGB(minhasher.numBytes()) << " GB." << std::endl;
 
-    std::cout << "begin correct" << std::endl;
-
     correct<Minhasher_t,
 			ReadStorage_t,
 			indelAlignment>(minhashOptions, alignmentOptions,
