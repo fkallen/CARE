e6c6d2be943ee09444210d010e3fe60c490deaac
Felix Kallenborn <kallenborn@uni-mainz.de>
2018-10-10 16:16:35 +0200

    disable unified memory readstorage

--- inc/gpu_only_path/readstorage_gpu.hpp
+++ inc/gpu_only_path/readstorage_gpu.hpp
@@ -35,7 +35,7 @@ namespace care{
 };*/
 
 struct GPUReadStorage{
-	
+
 	enum class Type{
 		None,
 		Full,
@@ -50,7 +50,7 @@ struct GPUReadStorage{
 	bool useQualityScores = false;
 
 	//GPUReadStorageType type = GPUReadStorageType::None;
-	
+
 	GPUReadStorage::Type sequenceType = GPUReadStorage::Type::None;
 	GPUReadStorage::Type qualityType = GPUReadStorage::Type::None;
 
@@ -62,35 +62,35 @@ struct GPUReadStorage{
             default: return "Error. GPUReadStorage::nameOf default case";
         }
     }
-    
+
     std::string getNameOfSequenceType() const{
 		return nameOf(sequenceType);
 	}
-	
+
 	std::string getNameOfQualityType() const{
 		return nameOf(qualityType);
 	}
-	
+
 	bool hasSequences() const{
 		return sequenceType != GPUReadStorage::Type::None;
 	}
-	
+
 	bool hasFullSequences() const{
 		return sequenceType == GPUReadStorage::Type::Full;
 	}
-	
+
 	bool hasManagedSequences() const{
 		return sequenceType == GPUReadStorage::Type::Managed;
 	}
-	
+
 	bool hasQualities() const{
 		return useQualityScores && qualityType != GPUReadStorage::Type::None;
 	}
-	
+
 	bool hasFullQualities() const{
 		return useQualityScores && qualityType == GPUReadStorage::Type::Full;
 	}
-	
+
 	bool hasManagedQualities() const{
 		return useQualityScores && qualityType == GPUReadStorage::Type::Managed;
 	}
@@ -113,12 +113,12 @@ struct GPUReadStorage{
         int oldId;
         cudaGetDevice(&oldId); CUERR;
         cudaSetDevice(deviceId); CUERR;
-		
+
 		cudaDeviceProp prop;
 		cudaGetDeviceProperties(&prop, deviceId);
-		
+
 		bool isCapableOfUsingManagedMemory = prop.concurrentManagedAccess == 1;
-		
+
 		std::size_t freeMem;
         std::size_t totalMem;
         cudaMemGetInfo(&freeMem, &totalMem); CUERR;
@@ -130,19 +130,19 @@ struct GPUReadStorage{
         gpurs.max_sequence_bytes = max_sequence_bytes;
         gpurs.max_sequence_length = max_sequence_length;
 		gpurs.useQualityScores = cpurs.useQualityScores;
-		
+
 		const std::uint64_t requiredSequenceMem = max_sequence_bytes * nSequences;
 		const std::uint64_t requiredQualityMem = cpurs.useQualityScores ? max_sequence_length * nSequences : 0;
 		const std::uint64_t requiredTotalMem = requiredSequenceMem + requiredQualityMem;
-		
+
 		const bool isEnoughMemForSequences = (requiredSequenceMem < maxPercentOfTotalMem * totalMem && requiredSequenceMem < freeMem);
 		const bool isEnoughMemForSequencesAndQualities = (requiredTotalMem < maxPercentOfTotalMem * totalMem && requiredTotalMem < freeMem);
-		
+
 		bool canTestSequences = false;
-		
+
 		//make sequences in gpu mem
-		if(isEnoughMemForSequences){			
-			
+		if(isEnoughMemForSequences){
+
 			constexpr std::uint64_t maxcopybatchsequences = std::size_t(10000000);
 
 			//copy sequences to GPU
@@ -185,38 +185,38 @@ struct GPUReadStorage{
 
 			cudaDeviceSynchronize(); CUERR;
 
-			cudaFreeHost(h_tmp); CUERR;		
-			
+			cudaFreeHost(h_tmp); CUERR;
+
 			gpurs.sequenceType = GPUReadStorage::Type::Full;
-			
+
 			canTestSequences = true;
 		}else{
 			//use managed memory
-			if(isCapableOfUsingManagedMemory && canUseManagedMemory){
+			if(false && isCapableOfUsingManagedMemory && canUseManagedMemory){
 				const std::uint64_t requiredSequenceMem = max_sequence_bytes * nSequences;
 				cudaMallocManaged(&gpurs.d_sequence_data, requiredSequenceMem); CUERR;
-				
-				cudaMemAdvise(gpurs.d_sequence_data, 
-								requiredSequenceMem, 
-								cudaMemAdviseSetReadMostly, 
+
+				cudaMemAdvise(gpurs.d_sequence_data,
+								requiredSequenceMem,
+								cudaMemAdviseSetReadMostly,
 								0); CUERR; //last argument is ignored for cudaMemAdviseSetReadMostly
-				
+
 				for(ReadId_t readId = 0; readId < nSequences; ++readId){
 					const Sequence_t* sequence = cpurs.fetchSequence_ptr(readId);
 
 					assert(sequence->getNumBytes() <= max_sequence_bytes);
-						
+
 					std::memcpy(gpurs.d_sequence_data + readId * max_sequence_bytes,
 								sequence->begin(),
 								sequence->getNumBytes());
-				}	
-				
+				}
+
 				gpurs.sequenceType = GPUReadStorage::Type::Managed;
-				
+
 				canTestSequences = true;
 			}
 		}
-		
+
 		if(canTestSequences){
 			//test code
 #if 0
@@ -251,11 +251,11 @@ struct GPUReadStorage{
 				cudaFree(d_test); CUERR;
 				cudaFreeHost(h_test); CUERR;
 			}
-#endif				
+#endif
 		}
-		
-		bool canTestQualities = false;		
-		
+
+		bool canTestQualities = false;
+
 		if(cpurs.useQualityScores){
 			if(isEnoughMemForSequencesAndQualities){
 				constexpr std::uint64_t maxcopybatchsequences = std::size_t(1000000);
@@ -300,36 +300,36 @@ struct GPUReadStorage{
 				cudaDeviceSynchronize(); CUERR;
 
 				cudaFreeHost(h_tmp); CUERR;
-				
+
 				gpurs.qualityType = GPUReadStorage::Type::Full;
-				
+
 				canTestQualities = true;
 			}else{
 				//use managed memory
 				if(isCapableOfUsingManagedMemory && canUseManagedMemory){
 					cudaMallocManaged(&gpurs.d_quality_data, requiredQualityMem); CUERR;
-					
-					cudaMemAdvise(gpurs.d_quality_data, 
-									requiredQualityMem, 
-									cudaMemAdviseSetReadMostly, 
+
+					cudaMemAdvise(gpurs.d_quality_data,
+									requiredQualityMem,
+									cudaMemAdviseSetReadMostly,
 									0); CUERR; //last argument is ignored for cudaMemAdviseSetReadMostly
-					
+
 					for(ReadId_t readId = 0; readId < nSequences; ++readId){
 						const std::string* qualityptr = cpurs.fetchQuality_ptr(readId);
 
 						assert(int(qualityptr->size()) <= max_sequence_length);
-							
+
 						std::memcpy(gpurs.d_quality_data + readId * max_sequence_length,
 									qualityptr->data(),
 									qualityptr->size());
-					}	
-					
+					}
+
 					gpurs.qualityType = GPUReadStorage::Type::Managed;
-					
+
 					canTestQualities = true;
 				}
 			}
-			
+
 			if(canTestQualities){
 				//test code
 	#if 0
@@ -364,7 +364,7 @@ struct GPUReadStorage{
 					cudaFree(d_test); CUERR;
 					cudaFreeHost(h_test); CUERR;
 				}
-	#endif				
+	#endif
 			}
 		}
 
@@ -378,7 +378,7 @@ struct GPUReadStorage{
 
 
 
-    
+
     template<class CPUReadStorage>
     static GPUReadStorageType getBestPossibleType(const CPUReadStorage& cpurs,
                                                     int max_sequence_bytes,
