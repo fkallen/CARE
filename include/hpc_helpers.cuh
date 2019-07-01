777647743ee298945f8a15ab951d2fbdab03f30d
Felix Kallenborn <kallenborn@uni-mainz.de>
2019-07-01 09:28:28 +0200

    only define macros if they are not defined yet

--- include/hpc_helpers.cuh
+++ include/hpc_helpers.cuh
@@ -5,54 +5,77 @@
 #include <cstdint>
 #include <chrono>
 
-#ifdef __CUDACC__
-    #define HOSTDEVICEQUALIFIER  __host__ __device__
-#else
-    #define HOSTDEVICEQUALIFIER
+#ifndef HOSTDEVICEQUALIFIER
+    #ifdef __CUDACC__
+        #define HOSTDEVICEQUALIFIER  __host__ __device__
+    #else
+        #define HOSTDEVICEQUALIFIER
+    #endif
 #endif
 
-#ifdef __CUDACC__
-    #define INLINEQUALIFIER  __forceinline__
-#else
-    #define INLINEQUALIFIER inline
+#ifndef INLINEQUALIFIER
+    #ifdef __CUDACC__
+        #define INLINEQUALIFIER  __forceinline__
+    #else
+        #define INLINEQUALIFIER inline
+    #endif
 #endif
 
-#ifdef __CUDACC__
-    #define GLOBALQUALIFIER  __global__
-#else
-    #define GLOBALQUALIFIER
+#ifndef GLOBALQUALIFIER
+    #ifdef __CUDACC__
+        #define GLOBALQUALIFIER  __global__
+    #else
+        #define GLOBALQUALIFIER
+    #endif
 #endif
 
-#ifdef __CUDACC__
-    #define DEVICEQUALIFIER  __device__
-#else
-    #define DEVICEQUALIFIER
+#ifndef DEVICEQUALIFIER
+    #ifdef __CUDACC__
+        #define DEVICEQUALIFIER  __device__
+    #else
+        #define DEVICEQUALIFIER
+    #endif
 #endif
 
-#ifdef __CUDACC__
-    #define HOSTQUALIFIER  __host__
-#else
-    #define HOSTQUALIFIER
+#ifndef HOSTQUALIFIER
+    #ifdef __CUDACC__
+        #define HOSTQUALIFIER  __host__
+    #else
+        #define HOSTQUALIFIER
+    #endif
 #endif
 
-#ifdef __CUDACC__
-    #define HD_WARNING_DISABLE #pragma hd_warning_disable
-#else
-    #define HD_WARNING_DISABLE
+#ifndef HD_WARNING_DISABLE
+    #ifdef __CUDACC__
+        #define HD_WARNING_DISABLE #pragma hd_warning_disable
+    #else
+        #define HD_WARNING_DISABLE
+    #endif
 #endif
 
 
+#ifndef TIMERSTARTCPU
     #define TIMERSTARTCPU(label)                                                  \
         std::chrono::time_point<std::chrono::system_clock> a##label, b##label; \
         a##label = std::chrono::system_clock::now();
+#endif
 
+#ifndef TIMERSTARTCPU
     #define TIMERSTOPCPU(label)                                                   \
         b##label = std::chrono::system_clock::now();                           \
         std::chrono::duration<double> delta##label = b##label-a##label;        \
         std::cout << "# elapsed time ("<< #label <<"): "                       \
                   << delta##label.count()  << " s" << std::endl;
+#endif
 
 
+
+
+
+
+
+
+#ifndef TIMERSTART
 
 #ifndef __CUDACC__
     #define TIMERSTART(label)                                                  \
@@ -66,6 +89,10 @@
         cudaEventCreate(&stop##label);                                         \
         cudaEventRecord(start##label, 0);
 #endif
+
+#endif
+
+#ifndef TIMERSTOP
 
 #ifndef __CUDACC__
     #define TIMERSTOP(label)                                                   \
@@ -82,7 +109,9 @@
                       << time##label << " ms" << std::endl;
 #endif
 
+#endif
 
+#ifndef CUERR
 
     #define CUERR {                                                            \
         cudaError_t err;                                                       \
@@ -93,13 +122,28 @@
         }                                                                      \
     }
 
+#endif
+
     // transfer constants
-    #define H2D (cudaMemcpyHostToDevice)
-    #define D2H (cudaMemcpyDeviceToHost)
-    #define H2H (cudaMemcpyHostToHost)
-    #define D2D (cudaMemcpyDeviceToDevice)
+    #ifndef H2D
+        #define H2D (cudaMemcpyHostToDevice)
+    #endif
+    #ifndef D2H
+        #define D2H (cudaMemcpyDeviceToHost)
+    #endif
+    #ifndef H2H
+        #define H2H (cudaMemcpyHostToHost)
+    #endif
+    #ifndef D2D
+        #define D2D (cudaMemcpyDeviceToDevice)
+    #endif
+    #ifndef SDIV
+        #define SDIV(x,y)(((x)+(y)-1)/(y))
+    #endif
+
+
 
     // safe division
-    #define SDIV(x,y)(((x)+(y)-1)/(y))
+
 
 #endif
