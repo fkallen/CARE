a6311ab43e4fb193ddf780e00f88604cfa4209a5
Felix Kallenborn <kallenborn@uni-mainz.de>
2019-10-21 14:55:50 +0200

    update build_cpu to the state of build_gpu

--- src/build.cpp
+++ src/build.cpp
@@ -11,6 +11,8 @@
 #include "sequence.hpp"
 #include "threadsafe_buffer.hpp"
 
+#include "threadpool.hpp"
+
 #include <stdexcept>
 #include <iostream>
 #include <limits>
@@ -27,7 +29,7 @@
 namespace care{
 
 
-
+#if 0
     BuiltDataStructure<cpu::ContiguousReadStorage> build_readstorage(const FileOptions& fileOptions,
                                                 const RuntimeOptions& runtimeOptions,
                                                 bool useQualityScores,
@@ -111,6 +113,226 @@ namespace care{
         }
 
     }
+
+#else 
+
+BuiltDataStructure<cpu::ContiguousReadStorage> build_readstorage(const FileOptions& fileOptions,
+                                                const RuntimeOptions& runtimeOptions,
+                                                bool useQualityScores,
+                                                read_number expectedNumberOfReads,
+                                                int expectedMinimumReadLength,
+                                                int expectedMaximumReadLength){
+
+
+
+        if(fileOptions.load_binary_reads_from != ""){
+            BuiltDataStructure<cpu::ContiguousReadStorage> result;
+            auto& readStorage = result.data;
+
+            readStorage.loadFromFile(fileOptions.load_binary_reads_from);
+            result.builtType = BuiltType::Loaded;
+
+            if(useQualityScores && !readStorage.hasQualityScores())
+                throw std::runtime_error("Quality scores are required but not present in compressed sequence file!");
+            if(!useQualityScores && readStorage.hasQualityScores())
+                std::cerr << "Warning. The loaded compressed read file contains quality scores, but program does not use them!\n";
+
+            std::cout << "Loaded binary reads from " << fileOptions.load_binary_reads_from << std::endl;
+
+            return result;
+        }else{
+            //int nThreads = std::max(1, std::min(runtimeOptions.threads, 4));
+
+            constexpr std::array<char, 4> bases = {'A', 'C', 'G', 'T'};
+            //int Ncount = 0;
+
+            BuiltDataStructure<cpu::ContiguousReadStorage> result;
+            auto& readStorage = result.data;
+
+            readStorage= std::move(cpu::ContiguousReadStorage{expectedNumberOfReads, useQualityScores, expectedMaximumReadLength});
+            result.builtType = BuiltType::Constructed;
+
+            auto checkRead = [&](read_number readIndex, Read& read, int& Ncount){
+                const int readLength = int(read.sequence.size());
+
+                if(readIndex >= expectedNumberOfReads){
+                    throw std::runtime_error("Error! Expected " + std::to_string(expectedNumberOfReads)
+                                            + " reads, but file contains at least "
+                                            + std::to_string(readIndex+1) + " reads.");
+                }
+
+                if(readLength > expectedMaximumReadLength){
+                    throw std::runtime_error("Error! Expected maximum read length = "
+                                            + std::to_string(expectedMaximumReadLength)
+                                            + ", but read " + std::to_string(readIndex)
+                                            + "has length " + std::to_string(readLength));
+                }
+
+                auto isValidBase = [](char c){
+                    constexpr std::array<char, 10> validBases{'A','C','G','T','a','c','g','t'};
+                    return validBases.end() != std::find(validBases.begin(), validBases.end(), c);
+                };
+
+                const int undeterminedBasesInRead = std::count_if(read.sequence.begin(), read.sequence.end(), [&](char c){
+                    return !isValidBase(c);
+                });
+
+                //nmap[undeterminedBasesInRead]++;
+
+                if(undeterminedBasesInRead > 0){
+                    readStorage.setReadContainsN(readIndex, true);
+                }
+
+                for(auto& c : read.sequence){
+                    if(c == 'a') c = 'A';
+                    else if(c == 'c') c = 'C';
+                    else if(c == 'g') c = 'G';
+                    else if(c == 't') c = 'T';
+                    else if(!isValidBase(c)){
+                        c = bases[Ncount];
+                        Ncount = (Ncount + 1) % 4;
+                    }
+                }
+            };
+
+
+            constexpr size_t maxbuffersize = 1000000;
+            constexpr int numBuffers = 2;
+
+            std::chrono::time_point<std::chrono::system_clock> tpa, tpb;
+            std::chrono::duration<double> duration;
+            std::uint64_t countlimit = 1000000;
+		    std::uint64_t count = 0;
+		    std::uint64_t totalCount = 0;
+
+            std::array<std::vector<read_number>, numBuffers> indicesBuffers;
+            std::array<std::vector<Read>, numBuffers> readsBuffers;
+            std::array<bool, numBuffers> canBeUsed;
+            std::array<std::mutex, numBuffers> mutex;
+            std::array<std::condition_variable, numBuffers> cv;
+
+            for(int i = 0; i < numBuffers; i++){
+                indicesBuffers[i].reserve(maxbuffersize);
+                readsBuffers[i].reserve(maxbuffersize);
+                canBeUsed[i] = true;
+            }
+
+            int bufferindex = 0;
+
+            tpa = std::chrono::system_clock::now();
+
+            auto updateProgress = [](auto totalCount, auto seconds){
+                std::cout << "Processed " << totalCount << " reads in file. Elapsed time: " 
+                            << seconds << " seconds." << std::endl;
+            };
+
+            forEachReadInFile(fileOptions.inputfile,
+                            fileOptions.format,
+                            [&](auto readnum, const auto& read){
+
+                    if(!canBeUsed[bufferindex]){
+                        std::unique_lock<std::mutex> ul(mutex[bufferindex]);
+                        if(!canBeUsed[bufferindex]){
+                            //std::cerr << "waiting for other buffer\n";
+                            cv[bufferindex].wait(ul, [&](){ return canBeUsed[bufferindex]; });
+                        }
+                    }
+
+                    auto indicesBufferPtr = &indicesBuffers[bufferindex];
+                    auto readsBufferPtr = &readsBuffers[bufferindex];
+                    indicesBufferPtr->emplace_back(readnum);
+                    readsBufferPtr->emplace_back(read);
+
+                    ++count;
+                    ++totalCount;
+
+                    if(count == countlimit){
+                        tpb = std::chrono::system_clock::now();
+                        duration = tpb - tpa;
+                        updateProgress(totalCount, duration.count());
+                        countlimit *= 2;
+                    }
+            
+
+                    if(indicesBufferPtr->size() >= maxbuffersize){
+                        canBeUsed[bufferindex] = false;
+
+                        //std::cerr << "launch other thread\n";
+                        threadpool.enqueue([&, indicesBufferPtr, readsBufferPtr, bufferindex](){
+                            //std::cerr << "buffer " << bufferindex << " running\n";
+                            int nmodcounter = 0;
+
+                            for(int i = 0; i < int(readsBufferPtr->size()); i++){
+                                read_number readId = (*indicesBufferPtr)[i];
+                                auto& read = (*readsBufferPtr)[i];
+                                checkRead(readId, read, nmodcounter);
+                                readStorage.insertRead(readId, read.sequence, read.quality);
+                            }
+
+                            //TIMERSTARTCPU(clear);
+                            indicesBufferPtr->clear();
+                            readsBufferPtr->clear();
+                            //TIMERSTOPCPU(clear);
+                            
+                            std::lock_guard<std::mutex> l(mutex[bufferindex]);
+                            canBeUsed[bufferindex] = true;
+                            cv[bufferindex].notify_one();
+
+                            //std::cerr << "buffer " << bufferindex << " finished\n";
+                        });
+
+                        bufferindex = (bufferindex + 1) % numBuffers; //swap buffers
+                    }
+
+            });
+
+            auto indicesBufferPtr = &indicesBuffers[bufferindex];
+            auto readsBufferPtr = &readsBuffers[bufferindex];
+
+            if(int(readsBufferPtr->size()) > 0){
+                if(!canBeUsed[bufferindex]){
+                    std::unique_lock<std::mutex> ul(mutex[bufferindex]);
+                    if(!canBeUsed[bufferindex]){
+                        //std::cerr << "waiting for other buffer\n";
+                        cv[bufferindex].wait(ul, [&](){ return canBeUsed[bufferindex]; });
+                    }
+                }
+
+                int nmodcounter = 0;
+
+                for(int i = 0; i < int(readsBufferPtr->size()); i++){
+                    read_number readId = (*indicesBufferPtr)[i];
+                    auto& read = (*readsBufferPtr)[i];
+                    checkRead(readId, read, nmodcounter);
+                    readStorage.insertRead(readId, read.sequence, read.quality);
+                }
+
+                indicesBufferPtr->clear();
+                readsBufferPtr->clear();
+            }
+
+            for(int i = 0; i < numBuffers; i++){
+                std::unique_lock<std::mutex> ul(mutex[i]);
+                if(!canBeUsed[i]){
+                    //std::cerr << "Reading file completed. Waiting for buffer " << i << "\n";
+                    cv[i].wait(ul, [&](){ return canBeUsed[i]; });
+                }
+            }
+
+            if(count > 0){
+                tpb = std::chrono::system_clock::now();
+                duration = tpb - tpa;
+                updateProgress(totalCount, duration.count());
+            }
+
+            return result;
+        }
+
+    }
+
+
+
+#endif
 
 
     BuiltDataStructure<Minhasher> build_minhasher(const FileOptions& fileOptions,
