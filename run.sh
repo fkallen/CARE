a96a275edfcfcab2d4de5912caf792036b00ff18
Felix Kallenborn <kallenborn@uni-mainz.de>
2019-12-10 14:22:57 +0100

    modify run script

--- run.sh
+++ run.sh
@@ -32,14 +32,16 @@ tempdir="/home/fekallen/storage/temp/"
 mkdir -p $outputdir
 mkdir -p $tempdir
 
+k=20
+
 echo "$care --inputfile=$inputfile --tempdir=$tempdir --outdir=$outputdir --outfile=$outputfilenamenopath --threads=$numthreads \
-      --hashmaps=48 --kmerlength=32 --batchsize=1000 --maxmismatchratio=0.20 --minalignmentoverlap=20 --minalignmentoverlapratio=0.20 \
+      --hashmaps=48 --kmerlength=$k --batchsize=1000 --maxmismatchratio=0.20 --minalignmentoverlap=20 --minalignmentoverlapratio=0.20 \
       --useQualityScores=true --coverage=$coverage --errorrate=0.06 --m_coverage=0.6 \
       --candidateCorrection=true --candidateCorrectionNewColumns=15 --extractFeatures=false --deviceIds=0 --correctionType=0 \
       --progress=true --nReads=0 --min_length=0 --max_length=0 --hits_per_candidate=1"
 
 $care --inputfile=$inputfile --tempdir=$tempdir --outdir=$outputdir --outfile=$outputfilenamenopath --threads=$numthreads \
-      --hashmaps=48 --kmerlength=32 --batchsize=1000 --maxmismatchratio=0.20 --minalignmentoverlap=20 --minalignmentoverlapratio=0.20 \
+      --hashmaps=48 --kmerlength=$k --batchsize=1000 --maxmismatchratio=0.20 --minalignmentoverlap=20 --minalignmentoverlapratio=0.20 \
       --useQualityScores=true --coverage=$coverage --errorrate=0.06 --m_coverage=0.6 \
       --candidateCorrection=true --candidateCorrectionNewColumns=15 --extractFeatures=false --deviceIds=0 --correctionType=0 \
       --progress=true --nReads=0 --min_length=0 --max_length=0 --hits_per_candidate=1
