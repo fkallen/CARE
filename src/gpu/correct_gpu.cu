81a11e1a5cc89c74aac119d3b98223fef0478a41
Felix Kallenborn <kallenborn@uni-mainz.de>
2020-02-24 12:48:57 +0100

    debug

--- src/gpu/correct_gpu.cu
+++ src/gpu/correct_gpu.cu
@@ -2734,22 +2734,22 @@ namespace gpu{
                 transFuncData.correctionStatusFlagsPerRead[readId] |= readCouldNotBeCorrectedAsAnchor;
             }
 
-            // if(readId == 13158000){
-            //     std::cerr << "readid = 13158000, stats\n";
-            //     std::cerr << "isCorrected " << isCorrected << ", isHQ " << isHQ << "\n";
-            //     auto& dataArrays = batch.dataArrays;
-            //     std::cerr << "num candidates " << dataArrays.h_candidates_per_subject[subject_index] 
-            //         << "num good candidates " << dataArrays.h_indices_per_subject[subject_index] << "\n";
-            //     std::cerr << "good candidate ids:\n";
+            if(readId == 37){
+                std::cerr << "readid = 37, stats\n";
+                std::cerr << "isCorrected " << isCorrected << ", isHQ " << isHQ << "\n";
+                auto& dataArrays = batch.dataArrays;
+                std::cerr << "num candidates " << dataArrays.h_candidates_per_subject[subject_index] 
+                    << "num good candidates " << rawResults.h_indices_per_subject[subject_index] << "\n";
+                std::cerr << "good candidate ids:\n";
 
-            //     const int globalOffset = rawResults.h_candidates_per_subject_prefixsum[subject_index];
+                const int globalOffset = rawResults.h_candidates_per_subject_prefixsum[subject_index];
 
-            //     for(int i = 0; i < dataArrays.h_indices_per_subject[subject_index]; i++){
-            //         const int index = dataArrays.h_indices[globalOffset + i];
-            //         const read_number candidateId = rawResults.h_candidate_read_ids[globalOffset + index];
-            //         std::cerr << candidateId << "\n";
-            //     }
-            // }
+                for(int i = 0; i < rawResults.h_indices_per_subject[subject_index]; i++){
+                    const int index = dataArrays.h_indices[globalOffset + i];
+                    const read_number candidateId = rawResults.h_candidate_read_ids[globalOffset + index];
+                    std::cerr << candidateId << "\n";
+                }
+            }
         }
 
         //int acc = 0;
@@ -2960,14 +2960,14 @@ namespace gpu{
                 tmpencoded = tmp.encode();
                 //TIMERSTOPCPU(encode);
 
-                // if(candidate_read_id == 13158000){
-                //     std::cerr << "readid = 13158000, as candidate of anchor with id " << subjectReadId << "\n";
-                //     std::cerr << "hq = " << tmp.hq << ", sequence = " << tmp.sequence;
-                //     std::cerr << "\nedits: ";
-                //     for(int i = 0; i < int(tmp.edits.size()); i++){
-                //         std::cerr << tmp.edits[i].base << ' ' << tmp.edits[i].pos << "\n";
-                //     }
-                // }
+                if(candidate_read_id == 37){
+                    std::cerr << "readid = 37, as candidate of anchor with id " << subjectReadId << "\n";
+                    std::cerr << "hq = " << tmp.hq << ", sequence = " << tmp.sequence;
+                    std::cerr << "\nedits: ";
+                    for(int i = 0; i < int(tmp.edits.size()); i++){
+                        std::cerr << tmp.edits[i].base << ' ' << tmp.edits[i].pos << "\n";
+                    }
+                }
             }
 
             nvtx::pop_range();
