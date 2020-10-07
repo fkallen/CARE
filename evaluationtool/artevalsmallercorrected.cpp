d6a9891b503f770f78099705718b012bdb32cf23
Felix Kallenborn <kallenborn@uni-mainz.de>
2020-10-07 15:06:14 +0200

    update evaluation scripts

--- evaluationtool/artevalsmallercorrected.cpp
+++ evaluationtool/artevalsmallercorrected.cpp
@@ -72,9 +72,8 @@ void forEachReadVectorInFiles(const std::vector<std::string>& filenames, Func&& 
             //std::cerr << "parser status = 0 in file " << filenames[i] << '\n';
             if(status >= 0){
                 reads[i].readNumber = readNumber;
-                //reads[i].name = readers[i]->getCurrentName();
-                //reads[i].comment = readers[i]->getCurrentComment();
-                reads[i].name = readers[i]->getCurrentHeader();
+                reads[i].name = readers[i]->getCurrentName();
+                reads[i].comment = readers[i]->getCurrentComment();
                 reads[i].sequence = readers[i]->getCurrentSequence();
                 reads[i].quality = readers[i]->getCurrentQuality();
             }else if(status < -1){
@@ -91,9 +90,8 @@ void forEachReadVectorInFiles(const std::vector<std::string>& filenames, Func&& 
                     //std::cerr << "parser status = 0 in file " << filenames[i] << '\n';
                     if(status >= 0){
                         reads[i].readNumber = readNumber;
-                        //reads[i].name = readers[i]->getCurrentName();
-                        //reads[i].comment = readers[i]->getCurrentComment();
-                        reads[i].name = readers[i]->getCurrentHeader();
+                        reads[i].name = readers[i]->getCurrentName();
+                        reads[i].comment = readers[i]->getCurrentComment();
                         reads[i].sequence = readers[i]->getCurrentSequence();
                         reads[i].quality = readers[i]->getCurrentQuality();
                     }else if(status < -1){
