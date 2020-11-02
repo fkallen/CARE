d764d19277ebf17d6a8c65de310002208be5eb7b
Julian Cascitti <jcascitt@students.uni-mainz.de>
2020-11-02 01:10:00 +0100

    added logistic regression clf

---
+++ include/deserialize.hpp
@@ -0,0 +1,28 @@
+#ifndef CARE_DESERIALIZE_HPP
+#define CARE_DESERIALIZE_HPP
+
+
+namespace care {
+
+// de-serialization helpers
+
+template<typename T>
+inline T& read_one(std::ifstream& is, T& v) {
+    char tmp[sizeof(v)];
+    // this is only to be absolutely 100% standard-compliant no matter how read() is implemented
+    // probably absolutely unnecessary but it will be optimized out
+    is.read(tmp, sizeof(v));
+    std::memcpy(&v, tmp, sizeof(v));
+    return v;
+}
+
+template<typename T>
+inline T read_one(std::ifstream& is) {
+    T ret;
+    read_one(is, ret);
+    return ret;
+}
+
+} // namespace care
+
+#endif
--- include/forest.hpp
+++ include/forest.hpp
@@ -7,27 +7,10 @@
 #include <vector>
 #include <cstring>
 #include <array>
+#include <deserialize.hpp>
 // #include <numeric>
 
 namespace care {
-
-// de-serialization helpers
-template<typename T>
-inline T& read_one(std::ifstream& is, T& v) {
-    char tmp[sizeof(v)];
-    // this is only to be absolutely 100% standard-compliant no matter how read() is implemented
-    // probably absolutely unnecessary but it will be optimized out
-    is.read(tmp, sizeof(v));
-    std::memcpy(&v, tmp, sizeof(v));
-    return v;
-}
-
-template<typename T>
-inline T read_one(std::ifstream& is) {
-    T ret;
-    read_one(is, ret);
-    return ret;
-}
 
 class ForestClf {
     template<class CpuForest>
