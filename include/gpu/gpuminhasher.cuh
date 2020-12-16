93b935910df388bf83224cab62c2137b607d2169
Felix Kallenborn <kallenborn@uni-mainz.de>
2020-12-16 12:12:15 +0100

    Add file for gpu minhasher interface

---
+++ include/gpu/gpuminhasher.cuh
@@ -0,0 +1,27 @@
+#ifndef CARE_GPUMINHASHER_CUH
+#define CARE_GPUMINHASHER_CUH
+
+
+#ifdef __NVCC__
+
+
+
+
+template<class Key_t, class Value_t>
+class GpuMinhasher{
+    using Key = Key_t;
+    using Value = Value_t;
+
+
+
+};
+
+
+
+
+
+
+#endif
+
+
+#endif
