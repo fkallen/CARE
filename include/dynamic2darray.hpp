3d393bd9a1f1cc548a0323673d8d2d934c8d52b3
Felix Kallenborn <kallenborn@uni-mainz.de>
2021-01-11 17:47:48 +0100

    bugfixes

--- include/dynamic2darray.hpp
+++ include/dynamic2darray.hpp
@@ -1,36 +1,47 @@
 #ifndef DYNAMIC_2D_ARRAY_HPP
 #define DYNAMIC_2D_ARRAY_HPP
 
-
 #include <vector>
 #include <cstddef>
 #include <cassert>
 #include <algorithm>
 #include <functional>
+#include <numeric>
+
 
 template<class T>
 class Dynamic2dArray{
 public:
+    using value_type = T;
+
     Dynamic2dArray(){
-        arraychunks.emplace_back(0);
+        offsetsPrefixSum.emplace_back(0);
     }
 
     void appendRow(const T* rowData, int length){
         assert(!isShaped);
+        
+        if(chunkArray.getNumChunks() == 0){
+            chunkArray.addChunk(getMemoryLimitChunk());
+            offsetsPrefixSum.emplace_back(0);
+        }
 
-        bool couldBeAppended = chunkArray.appendRow(getNumChunks() - 1, rowData, length);
+        bool couldBeAppended = chunkArray.appendRow(chunkArray.getNumChunks() - 1, rowData, length);
         if(!couldBeAppended){
             chunkArray.addChunk(getMemoryLimitChunk());
-            couldBeAppended = chunkArray.appendRow(getNumChunks() - 1, rowData, length);
+            couldBeAppended = chunkArray.appendRow(chunkArray.getNumChunks() - 1, rowData, length);
+            offsetsPrefixSum.emplace_back(offsetsPrefixSum.back() + 1);
 
             assert(couldBeAppended);
+        }else{
+            offsetsPrefixSum[chunkArray.getNumChunks()]++;
         }
     }
 
     void reshapeRows(std::size_t rowpitchelements){
         auto deallocVector = [](auto& vec){
-            using T = typename std::remove_reference<decltype(vec)>::type;
-            T tmp{};
+            using W = typename std::remove_reference<decltype(vec)>::type;
+            W tmp{};
             vec.swap(tmp);
         };
 
@@ -38,59 +49,158 @@ public:
 
             for(std::size_t i = 0; i < chunkArray.getNumChunks(); i++){
                 if(chunkArray.numRows[i] > 0){
-                    const int maxLength = *std::max_element(
+                    const std::size_t maxLength = *std::max_element(
                         chunkArray.rowLengths[i].begin(),
-                        chunkArray.rowLength[i].end(),
-                        std::min<int>{}
+                        chunkArray.rowLengths[i].end(),
+                        std::less<int>{}
                     );
 
                     std::size_t newpitchelements = std::max(rowpitchelements, maxLength);
 
-                    std::vector<T> newchunkdata(chunkArray.numRows[i] * newpitchelements);
+                    ShapedChunk newChunk{};
+                    newChunk.numRows = chunkArray.numRows[i];
+                    newChunk.rowPitchElements = newpitchelements;
+                    newChunk.data.resize(chunkArray.numRows[i] * newpitchelements);
 
                     auto inputiter = chunkArray.data[i].begin();
-                    auto outputiter = newchunkdata.begin();
+                    auto outputiter = newChunk.data.begin();
                     for(std::size_t row = 0; row < chunkArray.numRows[i]; row++){
-                        const int len = chunkArray.rowLength[i][row];
-                        outputiter = std::copy_n(inputiter, len, outputiter);
+                        const int len = chunkArray.rowLengths[i][row];
+                        std::copy_n(inputiter, len, outputiter);
+                        outputiter += newpitchelements;
                         inputiter += len;
                     }
 
-                    shapedChunkArray.data.emplace_back(std::move(newchunkdata));
-                    shapedChunkArray.numRows.emplace_back(chunkArray.numRows[i]);
-                    shapedChunkArray.rowPitchElements.emplace_back(newpitchelements);
+                    shapedChunkArray.emplace_back(std::move(newChunk));
 
                     deallocVector(chunkArray.data[i]);
-                    deallocVector(chunkArray.rowLength[i]);
+                    deallocVector(chunkArray.rowLengths[i]);
                 }
             }
+
+            chunkArray = std::move(ChunkArray{});
         }else{
-            for(std::size_t i = 0; i < shapedChunkArray.getNumChunks(); i++){
-                if(shapedChunkArray.data[i].size() > 0){
+            for(std::size_t i = 0; i < shapedChunkArray.size(); i++){
+                if(shapedChunkArray[i].numRows > 0){
 
-                    std::size_t newpitchelements = std::max(rowpitchelements, shapedChunkArray.rowPitchElements[i]);
+                    std::size_t newpitchelements = std::max(rowpitchelements, shapedChunkArray[i].rowPitchElements);
 
-                    std::vector<T> newchunkdata(shapedChunkArray.numRows[i] * newpitchelements);
+                    std::vector<T> newchunkdata(shapedChunkArray[i].numRows * newpitchelements);
 
-                    auto inputiter = shapedChunkArray.data[i].begin();
+                    auto inputiter = shapedChunkArray[i].data.begin();
                     auto outputiter = newchunkdata.begin();
-                    for(std::size_t row = 0; row < shapedChunkArray.numRows[i]; row++){
-                        const int len = shapedChunkArray.rowPitchElements[i];
+                    for(std::size_t row = 0; row < shapedChunkArray[i].numRows; row++){
+                        const int len = shapedChunkArray[i].rowPitchElements;
                         outputiter = std::copy_n(inputiter, len, outputiter);
                         inputiter += len;
                     }
 
-                    std::swap(shapedChunkArray.data[i], newchunkdata)
-                    shapedChunkArray.rowPitchElements[i] = newpitchelements;
+                    std::swap(shapedChunkArray[i].data, newchunkdata);
+                    shapedChunkArray[i].rowPitchElements = newpitchelements;
                 }
             }
         }
+
+        // offsetsPrefixSum.resize(shapedChunkArray.size() + 1);
+
+        // for(std::size_t i = 0; i < shapedChunkArray.size(); i++){
+        //     offsetsPrefixSum[i+1] = offsetsPrefixSum[i] + shapedChunkArray[i].numRows;
+        // }
 
         isShaped = true;
     }
 
     void reshapeRows(){
         reshapeRows(0);
+    }
+
+    template<class IndexGenerator>
+    void gather(
+        T* destination,
+        std::size_t numColumns,
+        IndexGenerator readIds,
+        int numReadIds,
+        size_t destinationPitchBytes
+    ) const noexcept{
+        
+        if(numReadIds == 0){
+            return;
+        }
+
+        if(!isShaped){
+            assert(false && "not implemented");
+        }else{
+            constexpr int prefetch_distance = 4;
+
+            for(int i = 0; i < numReadIds && i < prefetch_distance; ++i) {
+                const int index = i;
+                const std::size_t nextReadId = readIds(index);
+                const T* const nextData = getPointerToRow(nextReadId);
+                __builtin_prefetch(nextData, 0, 0);
+            }
+
+            for(int i = 0; i < numReadIds; i++){
+                if(i + prefetch_distance < numReadIds) {
+                    const int index = i + prefetch_distance;
+                    const std::size_t nextReadId = readIds(index);
+                    const T* const nextData = getPointerToRow(nextReadId);
+                    __builtin_prefetch(nextData, 0, 0);
+                }
+
+                const std::size_t readId = readIds(i);
+                const T* const data = getPointerToRow(readId);
+
+                T* const destData = (T*)(((char*)destination) + destinationPitchBytes * i);
+                std::copy_n(data, numColumns, destData);
+            }
+        }
+
+        
+    }
+
+    void getMemoryInfo() const {
+        if(isShaped){
+            std::size_t used = 0;
+
+            for(const auto& chunk : shapedChunkArray){
+                used += sizeof(T) * chunk.data.capacity();
+            }
+
+            used += shapedChunkArray.capacity() * sizeof(ShapedChunk);
+            used += offsetsPrefixSum.capacity() * sizeof(std::size_t);
+            std::cerr << "shaped: " << used << "\n";
+        }else{
+            std::size_t used = 0;
+
+            used += chunkArray.numRows.capacity() * sizeof(std::size_t);
+            used += chunkArray.memoryLimitData.capacity() * sizeof(std::size_t);
+            used += chunkArray.occupiedBytes.capacity() * sizeof(std::size_t);
+            used += chunkArray.dataend.capacity() * sizeof(typename std::vector<T>::iterator);
+            used += chunkArray.rowLengths.capacity() * sizeof(std::vector<int>);
+            used += chunkArray.data.capacity() * sizeof(std::vector<T>);
+
+            for(const auto& vec : chunkArray.rowLengths){
+                used += sizeof(int) * vec.capacity();
+            }
+
+            for(const auto& vec : chunkArray.data){
+                used += sizeof(T) * vec.capacity();
+            }
+            
+            std::cerr << "unshaped: " << used << "\n";
+        }
+    }
+
+    std::size_t getNumRows() const noexcept{
+        if(isShaped){
+            return offsetsPrefixSum.back();
+        }else{
+            return std::accumulate(
+                chunkArray.numRows.begin(), 
+                chunkArray.numRows.end(), 
+                std::size_t{0}
+            );
+        }
     }
 
 private:
@@ -109,9 +219,10 @@ private:
         bool appendRow(std::size_t chunk, const T* rowData, int length){
             const std::size_t extraBytes = sizeof(T) * length;
 
-            if(occupiedBytes[chunk] + extraBytes[chunk] <= memoryLimitData[chunk]){
+            if(occupiedBytes[chunk] + extraBytes <= memoryLimitData[chunk]){
                 dataend[chunk] = std::copy_n(rowData, length, dataend[chunk]);
                 rowLengths[chunk].emplace_back(length);
+                numRows[chunk]++;
 
                 occupiedBytes[chunk] += extraBytes;
                 return true;
@@ -132,27 +243,47 @@ private:
         std::vector<std::vector<T>> data{};
     };
 
-    struct ShapedChunkArray{
+    struct ShapedChunk{
+        const T* getPointerToRow(std::size_t rowIndex) const noexcept{
+            assert(rowIndex < numRows);
 
-        std::size_t getNumChunks() const noexcept{
-            return data.size();
+            return data.data() + rowIndex * rowPitchElements;
         }
 
         std::size_t numRows{};
-        std::vector<std::size_t> rowPitchElements{};
-        std::vector<std::vector<T>> data{};
+        std::size_t rowPitchElements{};
+        std::vector<T> data{};
     };
-
 
     constexpr std::size_t getMemoryLimitChunk() const noexcept{
         constexpr std::size_t MB = 1ull << 20;
-        return 64 * MB;
+        return 16 * MB;
+    }
+
+    std::size_t getChunkIndexOfRow(std::size_t row) const noexcept{
+        auto it = std::lower_bound(offsetsPrefixSum.begin(), offsetsPrefixSum.end(), row + 1);
+        std::size_t chunkIndex = std::distance(offsetsPrefixSum.begin(), it) - 1;
+
+        return chunkIndex;
+    }
+
+    std::size_t getRowIndexInChunk(std::size_t chunkIndex, std::size_t row) const noexcept{
+        return row - offsetsPrefixSum[chunkIndex];
+    }
+
+    const T* getPointerToRow(std::size_t row) const noexcept{
+        const std::size_t chunkIndex = getChunkIndexOfRow(row);
+        const std::size_t rowInChunk = getRowIndexInChunk(chunkIndex, row);
+
+        const T* const data = shapedChunkArray[chunkIndex].getPointerToRow(rowInChunk);
+        return data;
     }
 
     bool isShaped = false;
 
     ChunkArray chunkArray{};
-    ShapedChunkArray shapedChunkArray{};
+    std::vector<ShapedChunk> shapedChunkArray{};
+    std::vector<std::size_t> offsetsPrefixSum{};
 };
 
 
