9ace2a9b22e55c6d64cdef1fb6a77bc03c921931
Julian Cascitti <jcascitt@students.uni-mainz.de>
2021-03-10 13:40:41 +0100

    modified eval formatter for excel

--- ml/eval_formatter.py
+++ ml/eval_formatter.py
@@ -3,18 +3,37 @@
 from collections import defaultdict
 import sys
 
-data = defaultdict(lambda:defaultdict(lambda:defaultdict(lambda: defaultdict(lambda: ""))))
+data = {}
 for f in sys.argv[1:]:
     for line in open(f, "r"):
         if line.endswith(".fq\n"):
             anchor, cands = (int(x) for x in line[:-4].split('-')[-2:])
-        if line.startswith("TP"):
-            data[f][anchor][cands]["TP"] = int(line.split()[1])
-        if line.startswith("FP"):
-            data[f][anchor][cands]["FP"] = int(line.split()[1])
+        elif line.startswith("TP"):
+            tp = int(line.split()[1])
+        elif line.startswith("FP"):
+            fp = int(line.split()[1])
+            
+            to_delete = []
+            add = True
+            for setting in data:
+                if data[setting]["TP"]>=tp and data[setting]["FP"]<=fp:
+                    add = False
+                    break
+                elif data[setting]["TP"]<=tp and data[setting]["FP"]>=fp:
+                    to_delete.append(setting)
+            if add:
+                data[(f, anchor, cands)]={"TP":tp, "FP":fp}
+                for setting in to_delete:
+                    del data[setting]
 
-for anchor in range(0,101,5):
+min_thresh=60
+max_thresh=91
+step_thresh=5
+
+print("\t\t"+"\t".join(str(cands) for cands in range(min_thresh,max_thresh,step_thresh)))
+for anchor in range(min_thresh,max_thresh,step_thresh):
+    print(str(anchor), end="")
     for file in sys.argv[1:]:
-        print("\t".join(str(x) for x in (data[file][anchor][cands]["TP"] for cands in range(0,101,5))))
-        print("\t".join(str(x) for x in (data[file][anchor][cands]["FP"] for cands in range(0,101,5))))
+        print("\t\t"+"\t".join(str((data[(file,anchor,cands)]["TP"]) if (file,anchor,cands) in data else "") for cands in range(min_thresh,max_thresh,step_thresh)))
+        print("\t\t"+"\t".join(str((data[(file,anchor,cands)]["FP"]) if (file,anchor,cands) in data else "") for cands in range(min_thresh,max_thresh,step_thresh)))
             
