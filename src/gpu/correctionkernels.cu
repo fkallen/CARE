99b7c411a8158ebb8efedcda9bb6f0e2277a16b9
Felix Kallenborn <kallenborn@uni-mainz.de>
2021-03-12 11:03:54 +0100

    use group per sequence to construct correction results

--- src/gpu/correctionkernels.cu
+++ src/gpu/correctionkernels.cu
@@ -2203,7 +2203,8 @@ namespace gpu{
 
     //if isCompactCorrection == true, compare originalsequence[d_indicesOfCorrectedSequences[i]] with correctedsequence[i] to compute edits
     //if isCompactCorrection == false, compare originalsequence[d_indicesOfCorrectedSequences[i]] with correctedsequence[d_indicesOfCorrectedSequences[i]] to compute edits
-    template<bool isCompactCorrection>
+    //uses one group per sequence, 1 <= groupsize <= 32, groupsize is power of 2
+    template<bool isCompactCorrection, int groupsize>
     __global__
     void constructSequenceCorrectionResultsKernel(
         TempCorrectedSequence::EncodedEdit* __restrict__ d_edits,
@@ -2220,6 +2221,17 @@ namespace gpu{
         size_t decodedSequencePitchInBytes,
         size_t editsPitchInBytes
     ){
+        constexpr int maxblocksize = 256;
+        assert(blockDim.x <= maxblocksize);
+
+        __shared__ int sharedNumEdits[maxblocksize / groupsize];
+
+        auto threadblock = cg::this_thread_block();
+        auto group = cg::tiled_partition<groupsize>(threadblock);
+
+        const int globalGroupId = (threadIdx.x + blockIdx.x * blockDim.x) / groupsize;
+        const int numGroups = (blockDim.x * gridDim.x) / groupsize;
+        const int localGroupId = threadIdx.x / groupsize;
 
         auto to_nuc = [](std::uint8_t enc){
             return SequenceHelpers::decodeBase(enc);
@@ -2227,45 +2239,153 @@ namespace gpu{
 
         const int numIndicesToProcess = *d_numIndices;
 
-        for(int tid = threadIdx.x + blockIdx.x * blockDim.x; tid < numIndicesToProcess; tid += blockDim.x * gridDim.x){
-            const int indexOfUncorrected = d_indicesOfUncorrectedSequences[tid];
-            const int indexOfCorrected = isCompactCorrection ? tid : indexOfUncorrected;
-            const int indexOutput = tid;
+        for(int indexOutput = globalGroupId; indexOutput < numIndicesToProcess; indexOutput += numGroups){
+            const int indexOfUncorrected = d_indicesOfUncorrectedSequences[indexOutput];
+            const int indexOfCorrected = isCompactCorrection ? indexOutput : indexOfUncorrected;
 
             const bool thisSequenceContainsN = d_readContainsN[indexOfUncorrected];            
-            int* const myNumEdits = d_numEditsPerCorrection + indexOutput;
+            int* const myNumEditsGlobal = d_numEditsPerCorrection + indexOutput;
+            int* const myNumEditsShared = sharedNumEdits + localGroupId;
+
+            const unsigned int* const encodedUncorrectedSequence = d_uncorrectedEncodedSequences + encodedSequencePitchInInts * indexOfUncorrected;
+            const char* const decodedCorrectedSequence = d_correctedSequences + decodedSequencePitchInBytes * indexOfCorrected;
+
+            TempCorrectedSequence::EncodedEdit* const myEditsGlobal = (TempCorrectedSequence::EncodedEdit*)(((char*)d_edits) + editsPitchInBytes * indexOutput);
 
             if(thisSequenceContainsN){
-                *myNumEdits = doNotUseEditsValue;
+                if(group.thread_rank() == 0){
+                    *myNumEditsGlobal = doNotUseEditsValue;
+                }
             }else{
                 const int length = d_sequenceLengths[indexOfUncorrected];
 
-                //find correct pointers
-                const unsigned int* const encodedUncorrectedSequence = d_uncorrectedEncodedSequences + encodedSequencePitchInInts * indexOfUncorrected;
-                const char* const decodedCorrectedSequence = d_correctedSequences + decodedSequencePitchInBytes * indexOfCorrected;
-    
-                TempCorrectedSequence::EncodedEdit* const myEdits = (TempCorrectedSequence::EncodedEdit*)(((char*)d_edits) + editsPitchInBytes * tid);
+                if(group.thread_rank() == 0){
+                    *myNumEditsShared = 0;
+                }
+                group.sync();
 
                 const int maxEdits = min(length / 7, numEditsThreshold);
-                int edits = 0;
-                
-                for(int i = 0; i < length && edits <= maxEdits; i++){
-                    const char correctedNuc = decodedCorrectedSequence[i];
-                    const char uncorrectedNuc = to_nuc(SequenceHelpers::getEncodedNuc2Bit(encodedUncorrectedSequence, length, i));
 
-                    if(correctedNuc != uncorrectedNuc){
-                        if(edits < maxEdits){
-                            myEdits[edits] = TempCorrectedSequence::EncodedEdit{i, correctedNuc};
+                auto countAndSaveEditsWarp = [&](const int posInSequence, const char correctedNuc){
+                    cg::coalesced_group g = cg::coalesced_threads();
+                                
+                    int currentNumEdits = 0;
+                    if(g.thread_rank() == 0){
+                        currentNumEdits = atomicAdd(myNumEditsShared, g.size());
+                    }
+                    currentNumEdits = g.shfl(currentNumEdits, 0);
+    
+                    if(currentNumEdits + g.size() <= maxEdits){
+                        const int myEditOutputPos = g.thread_rank() + currentNumEdits;
+                        if(myEditOutputPos < maxEdits){
+                            const auto theEdit = TempCorrectedSequence::EncodedEdit{posInSequence, correctedNuc};
+                            myEditsGlobal[myEditOutputPos] = theEdit;
                         }
-                        edits++;
+                    }
+                };
+
+                auto countAndSaveEditsGroup = [&](const int posInSequence, const char correctedNuc){
+                    const int groupsPerWarp = 32 / group.size();
+                    if(groupsPerWarp == 1){
+                        countAndSaveEditsWarp(posInSequence, correctedNuc);
+                    }else{
+                        const int groupIdInWarp = (threadIdx.x % 32) / group.size();
+                        unsigned int subwarpmask = ((1u << (group.size() - 1)) | ((1u << (group.size() - 1)) - 1));
+                        subwarpmask <<= (group.size() * groupIdInWarp);
+
+                        unsigned int lanemask_lt;
+                        asm volatile("mov.u32 %0, %%lanemask_lt;" : "=r"(lanemask_lt));
+                        const unsigned int writemask = subwarpmask & __activemask();
+                        const unsigned int total = __popc(writemask);
+                        const unsigned int prefix = __popc(writemask & lanemask_lt);
+
+                        const int elected_lane = __ffs(writemask) - 1;
+                        int currentNumEdits = 0;
+                        if (prefix == 0) {
+                            currentNumEdits = atomicAdd(myNumEditsShared, total);
+                        }
+                        currentNumEdits = __shfl_sync(writemask, currentNumEdits, elected_lane);
+
+                        if(currentNumEdits + total <= maxEdits){
+                            const int myEditOutputPos = prefix + currentNumEdits;
+                            if(myEditOutputPos < maxEdits){
+                                const auto theEdit = TempCorrectedSequence::EncodedEdit{posInSequence, correctedNuc};
+                                myEditsGlobal[myEditOutputPos] = theEdit;
+                            }
+                        }
+
+                    }
+                };
+
+                constexpr int basesPerInt = SequenceHelpers::basesPerInt2Bit();
+                const int fullInts = length / basesPerInt;   
+                
+                for(int i = 0; i < fullInts; i++){
+                    const unsigned int encodedDataInt = encodedUncorrectedSequence[i];
+
+                    //compare with basesPerInt bases of corrected sequence
+                    for(int k = group.thread_rank(); k < basesPerInt; k += group.size()){
+                        const int posInInt = k;
+                        const int posInSequence = i * basesPerInt + posInInt;
+                        const std::uint8_t encodedUncorrectedNuc = SequenceHelpers::getEncodedNucFromInt2Bit(encodedDataInt, posInInt);
+                        const char correctedNuc = decodedCorrectedSequence[posInSequence];
+
+                        if(correctedNuc != to_nuc(encodedUncorrectedNuc)){
+                            countAndSaveEditsGroup(posInSequence, correctedNuc);
+                        }
+                    }
+
+                    group.sync();
+
+                    if(*myNumEditsShared > maxEdits){
+                        break;
                     }
                 }
-                if(edits <= maxEdits){
-                    *myNumEdits = edits;
+
+                //process remaining positions
+                if(*myNumEditsShared <= maxEdits){
+                    const int remainingPositions = length - basesPerInt * fullInts;
+
+                    if(remainingPositions > 0){
+                        const unsigned int encodedDataInt = encodedUncorrectedSequence[fullInts];
+                        for(int posInInt = group.thread_rank(); posInInt < remainingPositions; posInInt += group.size()){
+                            const int posInSequence = fullInts * basesPerInt + posInInt;
+                            const std::uint8_t encodedUncorrectedNuc = SequenceHelpers::getEncodedNucFromInt2Bit(encodedDataInt, posInInt);
+                            const char correctedNuc = decodedCorrectedSequence[posInSequence];
+
+                            if(correctedNuc != to_nuc(encodedUncorrectedNuc)){
+                                countAndSaveEditsGroup(posInSequence, correctedNuc);
+                            }
+                        }
+                    }
+                }
+
+                group.sync();
+
+                if(*myNumEditsShared <= maxEdits){
+                    if(group.thread_rank() == 0){ 
+                        *myNumEditsGlobal = *myNumEditsShared;
+                    }
+
+                    // const int fullInts = (numEdits * sizeof(TempCorrectedSequence::EncodedEdit)) / sizeof(int);
+                    // static_assert(sizeof(TempCorrectedSequence::EncodedEdit) * 2 == sizeof(int), "");
+
+                    // for(int i = tgroup.thread_rank(); i < fullInts; i += tgroup.size()) {
+                    //     ((int*)myEdits)[i] = ((int*)shared_Edits)[i];
+                    // }
+
+                    // for(int i = tgroup.thread_rank(); i < numEdits - fullInts * 2; i += tgroup.size()) {
+                    //     myEdits[fullInts * 2 + i] = shared_Edits[fullInts * 2 + i];
+                    // } 
                 }else{
-                    *myNumEdits = doNotUseEditsValue;
+                    if(group.thread_rank() == 0){
+                        *myNumEditsGlobal = doNotUseEditsValue;
+                    }
                 }
             }
+
+            group.sync(); //sync before handling next sequence
+                    
         }
     }
 
@@ -2734,15 +2854,16 @@ namespace gpu{
             stream
         ); CUERR;
 
+        constexpr int groupsize = 16;
+
         const int blocksize = 128;
         const std::size_t smem = 0;
 
         dim3 block = blocksize;
-        dim3 grid = SDIV(numCorrectedSequencesUpperBound, block.x);
-        const std::size_t smem = 0;
+        dim3 grid = SDIV(numCorrectedSequencesUpperBound, blocksize / groupsize);
 
         if(isCompactCorrection){
-            constructSequenceCorrectionResultsKernel<true><<<grid, block, smem, stream>>>(
+            constructSequenceCorrectionResultsKernel<true, groupsize><<<grid, block, smem, stream>>>(
                 d_edits,
                 d_numEditsPerCorrection,
                 doNotUseEditsValue,
@@ -2758,7 +2879,7 @@ namespace gpu{
                 editsPitchInBytes
             ); CUERR;
         }else{
-            constructSequenceCorrectionResultsKernel<false><<<grid, block, smem, stream>>>(
+            constructSequenceCorrectionResultsKernel<false, groupsize><<<grid, block, smem, stream>>>(
                 d_edits,
                 d_numEditsPerCorrection,
                 doNotUseEditsValue,
