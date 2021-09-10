d9c189d854ae1a82851ccfad89a98f09cae9c878
Felix Kallenborn <kallenborn@uni-mainz.de>
2021-09-10 09:27:00 +0200

    Error message shows if it was CUDACHECK or CUDACHECKASYNC which caught the error

--- include/gpu/cudaerrorcheck.cuh
+++ include/gpu/cudaerrorcheck.cuh
@@ -20,6 +20,7 @@
 
 #ifdef __CUDACC__
 
+#if 0
 #define CUDACHECK(ans) { \
     using namespace std::string_literals; \
     constexpr bool abort = true; \
@@ -33,8 +34,25 @@
         } \
     } \
 }
+#endif
 
-#define CUDACHECKASYNC { CUDACHECK((cudaPeekAtLastError())); }
+#define CUDACHECKIMPL(ans, async) { \
+    using namespace std::string_literals; \
+    constexpr bool abort = true; \
+    cudaError_t MY_CUDA_CHECK_status = (ans);                                 \
+    if (MY_CUDA_CHECK_status != cudaSuccess){              \
+        cudaGetLastError();                 \
+        std::string msg = (async ? "Asynchronous "s : "Synchronous "s) + "CUDA Error: "s + cudaGetErrorString(MY_CUDA_CHECK_status) + " "s + __FILE__ + " "s + std::to_string(__LINE__); \
+        std::cerr << msg << "\n"; \
+        if(abort){ \
+            throw std::runtime_error(msg); \
+        } \
+    } \
+}
+
+#define CUDACHECK(ans) { CUDACHECKIMPL((ans), false); }
+
+#define CUDACHECKASYNC { CUDACHECKIMPL((cudaPeekAtLastError()), true); }
 
 #else
 
