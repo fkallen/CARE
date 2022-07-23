d2b811613e04e25f564dd031f9ca805a886013d5
Felix Kallenborn <kallenborn@uni-mainz.de>
2022-07-23 16:55:43 +0200

    cleanup dependencies

--- dependencies/warpcore/utils/rebuild_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")/../docs"
-cp Doxyfile /tmp/
-rm -rf *
-mv /tmp/Doxyfile .
-doxygen Doxyfile
--- dependencies/warpcore/utils/update_catch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp -O "$(dirname "$0")/../tests/include/catch.hpp"
